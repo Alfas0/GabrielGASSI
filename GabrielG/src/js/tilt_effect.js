4882f7bd575cde345b606349586c0c88a052e6f8
Alfas0 <gabriel.murloc@gmail.com>
2025-05-28 21:21:48 +0200

    Final tilt_effect.js

--- GabrielG/src/js/tilt_effect.js
+++ GabrielG/src/js/tilt_effect.js
@@ -1,1 +1,19 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateX = ((y / rect.height) - 0.5) * -30;
+    const rotateY = ((x / rect.width) - 0.5) * 30;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.1)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)';
+  });
+});
 
