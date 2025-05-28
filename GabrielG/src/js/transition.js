771c59302f2982df51f426483338ed7f6fc09b29
Alfas0 <gabriel.murloc@gmail.com>
2025-05-28 21:25:37 +0200

    final transition.js

--- GabrielG/src/js/transition.js
+++ GabrielG/src/js/transition.js
@@ -1,1 +1,13 @@
-
+document.querySelectorAll('.transition-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.classList.remove('opacity-100');
+    document.body.classList.add('opacity-0');
+    setTimeout(() => {
+      window.location.href = this.href;
+    }, 500); 
+  });
+});
+window.addEventListener('pageshow', () => {
+  document.body.classList.add('opacity-100');
+});
