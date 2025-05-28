620cd21b5b81d745429647441f19922dd891956c
Alfas0 <gabriel.murloc@gmail.com>
2025-05-28 21:24:44 +0200

    final contact.js

--- GabrielG/src/js/contact.js
+++ GabrielG/src/js/contact.js
@@ -1,1 +1,19 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    const quizCompleted = localStorage.getItem("quizCompleted");
+    const modal = document.getElementById("quizModal");
 
+    if (!quizCompleted) {
+      modal.classList.remove("hidden");
+
+      const form = document.querySelector("form");
+      if (form) {
+        form.addEventListener("submit", function (e) {
+          e.preventDefault();
+        });
+      }
+
+      document.getElementById("closeModalBtn").addEventListener("click", () => {
+        window.location.href = "quiz.html";
+      });
+    }
+  });
