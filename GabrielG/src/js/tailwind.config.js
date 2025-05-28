95994d4cf22b3b19966644966f04a7678c26e5aa
Alfas0 <gabriel.murloc@gmail.com>
2025-05-28 21:25:15 +0200

    final tailwind.config.js

--- GabrielG/src/js/tailwind.config.js
+++ GabrielG/src/js/tailwind.config.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeInAnimation 0.4s ease-in forwards',
+      },
+      keyframes: {
+        fadeInAnimation: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
